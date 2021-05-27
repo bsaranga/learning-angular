f30f456867a3df96aa791f776f777915d1435189
bsaranga <bsaranga@gmail.com>
2021-05-27 17:01:42 +0530

    basic rxjs counter observable

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+  count: number = 0;
   title = 'http';
+
+  ngOnInit() {
+    this.initializeCounter();
+  }
+
+  initializeCounter():void {
+    const secondsCounter = interval(1000);
+    secondsCounter.subscribe(n => {
+      this.count = n;
+    });
+  }
 }
